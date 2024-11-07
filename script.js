3e8a1a4663e2221498e8b5a318b2c052edac2e75
Dezkiel <dezkiel@gmail.com>
2024-11-07 12:33:44 +0100

    Initial commit, getComputerChoice func created

---
+++ script.js
@@ -0,0 +1,21 @@
+function getComputerChoice(){
+    let rand = Math.floor((Math.random() * 3 + 1));
+
+    switch(rand)
+    {
+        case 1:
+            console.log("rock")
+            break;
+        case 2:
+            console.log("paper")
+            break;
+        case 3:
+            console.log("scissors")
+            break;
+        default:
+            console.log(rand);
+    }
+};
+
+getComputerChoice();
+
